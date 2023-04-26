5ae394189226c2b2a74d18894e5f8021dbd3a3c3
Ront31 <kolya.maslov.09@gmail.com>
2023-04-26 22:37:01 +0300

    task 3 done

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,13 +1,13 @@
 import css from './Friendslist.module.css';
 import PropTypes from 'prop-types';
 
-export const Friend = ({ avatar, name, isOnline, id }) => {
+export const Friend = ({ avatar, name, isOnline, id }) => (
   <li className={css.item}>
     <span className={isOnline ? css.online : css.offline}></span>
     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
     <p className={css.name}>{name}</p>
-  </li>;
-};
+  </li>
+);
 
 Friend.propTypes = {
   avatar: PropTypes.string.isRequired,
